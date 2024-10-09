cfad90f87bb880e6fbe345f11eb59c1989feffc4
firdavsrustamovv <rustamovfirdavs15@gmail.com>
2024-10-09 18:04:23 +0500

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./index.css";
+const App = ({ myimg, textImg }: { myimg: string; textImg: string }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div>
+        <img src={myimg} alt="BMW" />
+        <p>{textImg}</p>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
